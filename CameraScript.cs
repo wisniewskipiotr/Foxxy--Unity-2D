2b8ca4333a7d0cd0cac676d3921e054c395ba026
Szerys <wisnia1293@gmail.com>
2018-11-22 23:52:33 +0100

    Camera and PlayerMovment scripts

---
+++ CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+    public GameObject Camera;
+    public GameObject Player;
+    private float CameraZOffset;
+    private void FixedUpdate()
+    {
+        var PlayerPosition = Player.transform.position;
+        var CameraPosition = new Vector3(PlayerPosition.x, PlayerPosition.y, this.CameraZOffset);
+        Camera.transform.position = CameraPosition;
+        Debug.Log(PlayerPosition);
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        this.CameraZOffset = Camera.transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
