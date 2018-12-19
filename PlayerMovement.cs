a0aa1944a03c39acb91dcdd9fb040214386bdc22
Szerys <wisnia1293@gmail.com>
2018-12-19 20:27:52 +0100

    Updated PlayerMovment script and added patrol and MainMenu scripts

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -1,36 +1,91 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
     public CharacterController2D controller;
+    public Animator animator;
+    public CapsuleCollider2D capbox;
 
     public float runSpeed = 40f;
+    public Text scoreText;
+    int score;
 
     float horizontalMove = 0f;
     bool jump = false;
+    bool IsHurt = false;
 
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+
+        // Grab the components in case they weren't loaded properly
+        animator = GetComponent<Animator>();
+        controller = GetComponent<CharacterController2D>();
+
+        // initialize the start values
+        jump = false;
+        IsHurt = false;
+
+        score = 0;
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+
+        scoreText.text = score.ToString();
+
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
         if (Input.GetButtonDown("Jump"))
         {
-
-            jump = true; 
-
+            jump = true;
+            animator.SetBool("IsJumping", true);
         }
-	}
 
-    void FixedUpdate()
+
+    }
+
+    public void OnLanding()
     {
+        jump = false;
+        animator.SetBool("IsJumping", false);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Spikes")
         {
-
-            controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
-            jump = false;
-
+            jump = true;
+            animator.SetBool("IsHurt", true);
+            capbox.enabled = false;
         }
+        else if (col.gameObject.tag == "Enemy")
+        {
+            if (jump)
+            {
+                Destroy(col.gameObject);
+                score++;
+            }
+                
+            else
+            {
+                jump = true;
+                animator.SetBool("IsHurt", true);
+                capbox.enabled = false;
+
+            }
+        }
+    }
+            void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        
     }
 }
